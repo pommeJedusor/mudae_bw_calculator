d32f9fc475c5bc460cf5ba2ad67a1f2277f24727
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-03-15 21:36:23 +0100

    add graph

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
 MIN_NUMBER_OF_ROLL = 10
 
-def get_best_bw_value(base_nb_roll: int, base_wish_boost: float, is_star_wish: bool) -> int:
+def get_bw_total_rates(base_nb_roll: int, base_wish_boost: float, is_star_wish: bool) -> list[float]:
     # constant for the base probability to roll a given waifu/husbando
     c = 1
     wish_boost = base_wish_boost
     nb_roll = base_nb_roll
-    best = (-1, -1)
+    values: list[float] = []
     for i in range(base_nb_roll - MIN_NUMBER_OF_ROLL + 1):
-        best_rate, _ = best
         rate_by_roll = c * 100 + c * wish_boost
         total_rate = nb_roll * rate_by_roll
 
-        if total_rate > best_rate:
-            best = (total_rate, i)
+        values.append(total_rate)
 
         nb_roll -= 1
 
@@ -33,8 +34,15 @@ def get_best_bw_value(base_nb_roll: int, base_wish_boost: float, is_star_wish: b
             wish_boost += 5
         elif is_star_wish:
             wish_boost += 1
-    return best[1]
+    return values
 
+def show_graph(base_nb_roll: int, base_wish_boost: float, base_starwish_boost: float):
+    wish_values = get_bw_total_rates(base_nb_roll, base_wish_boost, False)
+    starwish_values = get_bw_total_rates(base_nb_roll, base_starwish_boost, True)
+    x = [i for i in range(len(wish_values))]
+    plt.plot(x, wish_values, label = "wish_values")
+    plt.plot(x, starwish_values, label = "starwish_values")
+    plt.legend()
+    plt.show()
 
-print(get_best_bw_value(13, 150, False))
-print(get_best_bw_value(13, 250, True))
+show_graph(14, 150, 250)
